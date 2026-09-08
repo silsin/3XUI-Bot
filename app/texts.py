3ac741c7a16da665840de108465af8c992632087
hasan <hasan@digilite.ca>
2026-09-08 10:29:53 +0330

    fix

--- app/texts.py
+++ app/texts.py
@@ -15,11 +15,11 @@ BTN_MY_WALLET = "💾 کیف داده"
 BTN_EARN = "💰 کسب درآمد"
 
 MAIN_BUTTONS = [
+    [BTN_EARN],
     [BTN_TRIAL, BTN_BUY],
     [BTN_RENEW, BTN_MY_SERVICES],
     [BTN_POINTS, BTN_GUIDE],
     [BTN_INVITE, BTN_SUPPORT],
-    [BTN_EARN],
 ]
 
 # ---------- کلیدهای تنظیمات ----------
